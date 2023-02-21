614b548de80b0db0c51e84d3a262d323992b92b5
Alex <alexvasilchenko2005@gmail.com>
2023-02-21 13:03:59 +0000

    Label Update

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Bachu',
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
